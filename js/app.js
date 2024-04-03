ec912a0d2ee30cffea9783450b4b4cfbc499adff
vikakvasova <159725202+vikakvasova@users.noreply.github.com>
2024-04-03 15:08:36 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,4 +4,18 @@ $('.burger-menu').click (function name(event) {
 
     $('.burger-menu, .navigation-list').toggleClass('active')
     
-})
+});
+new Swiper ('.slider', {
+    pagination:{
+        el:'.swiper-pagination',
+        clickable:true,
+    },
+    touchRatio: 2,
+    grabCursor: true,
+    loop: true,
+    autoplay:{
+ delay:3000,
+ disableOnInteraction: false,
+    },
+
+});
